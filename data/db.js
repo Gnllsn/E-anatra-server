34b369911bbad5ff71ae3f81a58770fe980508de
Gino Allison <77733677+Gnllsn@users.noreply.github.com>
2022-05-24 00:03:14 +0300

    update port

--- data/db.js
+++ data/db.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 
 module.exports = (app) =>{
 	const mongoose = require('mongoose');
+	const port = process.env.PORT || 3000 ;
 	const uri = process.env.DATABASE_URI;
 	mongoose.connect(uri, { useNewUrlParser: true });
 
 	const db = mongoose.connection;
 	db.on('error', (error)=> console.log('error'));
 	db.once('open', ()=> {
-		app.listen(3000,()=> console.log('server started eh')); 
+		app.listen(port,()=> console.log('server started eh')); 
 		console.log('Connected to database : '+db.name)
 	});
 };
